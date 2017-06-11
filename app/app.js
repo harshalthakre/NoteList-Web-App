191f1a279902b91a515671b43bbc65b32e6afdc4
harshalthakre <harshalthakre.07@gmail.com>
2017-06-11 15:25:52 +0530

    Adding new friend phase 1

--- app/app.js
+++ app/app.js
@@ -15,26 +15,47 @@ myNinjaApp.run(function(){
 
 //['$scope'] to protect it from minification version of angualr js
 myNinjaApp.controller('NinjaController',['$scope',function($scope){
+
+  $scope.removeNinja=function(ninja){
+    var removedNinja= $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja,1);
+  }
+
+  $scope.addNinja=function(){
+    $scope.ninjas.push({
+      name:$scope.newninja.name,
+      belt:$scope.newninja.belt,
+      rate:parseInt($scope.newninja.rate),
+      available:true
+    });
+  };
 $scope.message="hello buddies";
 $scope.greet="kya mamu";
+
+
 $scope.ninjas=[
 
   {
     name: "Nayan",
     belt: "green",
     rate: 80,
+    available:true,
   },
   {
     name:"Shubham",
     belt:"black",
     rate: 20,
+    available:false,
   },
   {
     name:"Harshal",
     belt:"yellow",
     rate: 100,
+    available:true,
   }
 
   ];
+
+
 
 }]);
