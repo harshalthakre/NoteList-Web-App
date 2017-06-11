6fc9537d41d0a86c171e3e1834a6dbaf43344fed
harshalthakre <harshalthakre.07@gmail.com>
2017-06-11 14:18:19 +0530

    Controllers

--- app/app.js
+++ app/app.js
@@ -1,30 +1,38 @@
-var myNinjaApp= angular.module('myNinjaApp',[]);
+var myNinjaApp= angular.module('myNinjaApp',[]); // 'myNinjaApp' and whatever ng-app name will be both should be same
 
+
+//before your application run it came here to see where to go
 myNinjaApp.config(function(){
 
 });
 
+
+// as ur application run
 myNinjaApp.run(function(){
 
 });
 
-myNinjaApp.controller('NinjaController',['$scope',function($scope){
 
+//['$scope'] to protect it from minification version of angualr js
+myNinjaApp.controller('NinjaController',['$scope',function($scope){
 $scope.message="hello buddies";
 $scope.greet="kya mamu";
 $scope.ninjas=[
 
   {
     name: "Nayan",
-    belt: "green"
+    belt: "green",
+    rate: 80,
   },
   {
     name:"Shubham",
-    belt:"black"
+    belt:"black",
+    rate: 20,
   },
   {
     name:"Harshal",
-    belt:"yellow"
+    belt:"yellow",
+    rate: 100,
   }
 
   ];
