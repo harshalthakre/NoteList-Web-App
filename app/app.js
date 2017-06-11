2f75352a2fe17590f5ac0b1bf526fc9254d7d952
harshalthakre <harshalthakre.07@gmail.com>
2017-06-11 23:07:20 +0530

    Added json file

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ myNinjaApp.config(['$routeProvider',function($routeProvider){
 
 
 //['$scope'] to protect it from minification version of angualr js
-myNinjaApp.controller('NinjaController',['$scope',function($scope){
+myNinjaApp.controller('NinjaController',['$scope','$http','$sce',function($scope,$http,$sce){
 
   $scope.removeNinja=function(ninja){
     var removedNinja= $scope.ninjas.indexOf(ninja);
@@ -45,34 +45,47 @@ myNinjaApp.controller('NinjaController',['$scope',function($scope){
 $scope.message="hello buddies";
 $scope.greet="kya mamu";
 
+/*$http.get('data/ninjas.json').success(function(data){
+ $scope.ninjas=data;
+ console.log("passed")
+});*/
 
-$scope.ninjas=[
+/*$http({
+     method: 'GET',
+     url: 'data/ninjas.json'
+  }).then(function (success){
+    $scope.ninjas=success.data;
+  },function (error){
 
-  {
-    name: "Nayan",
-    belt: "green",
-    rate: 80,
-    available:true,
-    thumb:"content/img/avatar3.png",
+  });*/
 
-  },
-  {
-    name:"Shubham",
-    belt:"black",
-    rate: 20,
-    available:false,
-    thumb:"content/img/avatar2.png",
-  },
-  {
-    name:"Harshal",
-    belt:"yellow",
-    rate: 100,
-    available:true,
-    thumb:"content/img/avatar2.png",
+  $http.get('data/ninjas.json').then(successCallback, errorCallback);
+
+  function successCallback(response){
+      //success code
+      // refer documentation for response object
+        $scope.ninjas=response.data;
+        console.log($scope.ninjas);
   }
+  function errorCallback(error){
+      //error code
+  }
+/*  $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCIswYVmH2GII181tNbYxTsOM7W57sPjcY').then(successCallback1, errorCallback1);
 
-  ];
-
+  function successCallback1(success){
+      //success code
+      // refer documentation for response object
+        $scope.genres=success.data;
+        console.log("in genres");
+        console.log("genres are: "+$scope.genres);
+  }
+  function errorCallback1(error){
+      //error code
+      $scope.genres=error.data;
+      console.log(error);
+      console.log("in genres ,m not");
+  }
+*/
 
 
 }]);
