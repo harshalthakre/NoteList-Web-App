81e8454ecdb081b97e0d5cb8cf4e0333c8269933
harshalthakre <harshalthakre.07@gmail.com>
2017-06-11 18:57:41 +0530

    Added route 1

--- app/app.js
+++ app/app.js
@@ -1,16 +1,23 @@
-var myNinjaApp= angular.module('myNinjaApp',[]); // 'myNinjaApp' and whatever ng-app name will be both should be same
+var myNinjaApp= angular.module('myNinjaApp',['ngRoute']); // 'myNinjaApp' and whatever ng-app name will be both should be same
 
 
 //before your application run it came here to see where to go
-myNinjaApp.config(function(){
-
-});
+myNinjaApp.config(['$routeProvider',function($routeProvider){
+  $routeProvider.
+  when('/home',{
+    templateUrl:'views/home.html'
+  })
+  .when('/directory',{
+    templateUrl:'views/directory.html',
+    controller: 'NinjaController'
+  }).otherwise({
+    redirectTo:'/home'
+  })
+}]);
 
 
 // as ur application run
-myNinjaApp.run(function(){
 
-});
 
 
 //['$scope'] to protect it from minification version of angualr js
